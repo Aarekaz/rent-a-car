33fb65b89ba8e8d908c786fa30f0b6d90c439633
Anurag Dhungana <anuragdhungana1@gmail.com>
2019-07-25 15:45:25 +0545

    Add files via upload

---
+++ database/migrations/2019_07_20_160051_create_car_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('car', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('brand');
+            $table->string('model');
+            $table->integer('In_Stock');
+            $table->longText('Car_Description');
+            $table->integer('cost');
+            $table->boolean('available');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('car');
+    }
+}
