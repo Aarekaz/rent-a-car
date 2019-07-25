7b1fa23854f47194a7f69b1d5e9d6d22b38a6f71
Anurag Dhungana <anuragdhungana1@gmail.com>
2019-07-25 15:57:30 +0545

    more fils

---
+++ storage/framework/views/2d0188e4b0bd91d0cdf49f8ecdc326430f014eb8.php
@@ -0,0 +1,363 @@
+	<!DOCTYPE html>
+	<html lang="zxx" class="no-js">
+	<head>
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Favicon-->
+		<link rel="shortcut icon" href="img/elements/fav.png">
+		<!-- Author Meta -->
+		<meta name="author" content="colorlib">
+		<!-- Meta Description -->
+		<meta name="description" content="">
+		<!-- Meta Keyword -->
+		<meta name="keywords" content="">
+		<!-- meta character set -->
+		<meta charset="UTF-8">
+		<!-- Site Title -->
+		<title>P4L Rent-A-Car Service</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+			<!--
+			CSS
+			============================================= -->
+			<link rel="stylesheet" href="css/linearicons.css">
+			<link rel="stylesheet" href="css/owl.carousel.css">
+			<link rel="stylesheet" href="css/font-awesome.min.css">
+			<link rel="stylesheet" href="css/nice-select.css">			
+			<link rel="stylesheet" href="css/magnific-popup.css">
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/main.css">
+		</head>
+		<body>
+
+			  <header id="header" id="home">
+			    <div class="container">
+			    	<div class="row align-items-center justify-content-between d-flex">
+				      <div id="logo">
+				        <a href="index.html"><img src="img/logo.png" alt="" title="" /></a>
+				      </div>
+				      <nav id="nav-menu-container">
+				       <ul class="nav-menu">
+                        <li class="menu-active"><a href="/">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/cars">Cars</a></li>
+                        <li><a href="/service">Service</a></li>
+                        <li><a href="/login">Login</a></li>
+                        <li><a href="/register">Register</a></li>
+
+
+                        <li><a href="/contact">Contact Us</a></li>
+
+                    </ul>
+				      </nav><!-- #nav-menu-container -->		    		
+			    	</div>
+			    </div>
+			  </header><!-- #header -->
+
+			<!-- start banner Area -->
+			<section class="banner-area relative" id="home">	
+				<div class="overlay overlay-bg"></div>
+				<div class="container">
+					<div class="row d-flex align-items-center justify-content-center">
+						<div class="about-content col-lg-12">
+							<h1 class="text-white">
+								About Us				
+							</h1>	
+							<p class="text-white link-nav"><a href="index.html">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="about.html"> About Us</a></p>
+						</div>											
+					</div>
+				</div>
+			</section>
+			<!-- End banner Area -->	
+
+			<!-- Start home-about Area -->
+			<section class="home-about-area" id="about">
+				<div class="container-fluid">				
+					<div class="row justify-content-center align-items-center">
+						<div class="col-lg-6 no-padding home-about-left">
+							<img class="img-fluid" src="img/about-img.jpg" alt="">
+						</div>
+						<div class="col-lg-6 no-padding home-about-right">
+							<h1>About Us</h1>
+							<p>
+								<span>We deliver excellence</span>
+							</p>
+							<p>
+								P4L rent a car is a car rental service, founded in Kathmandu, Nepal in 2019, and started out for renting cars for long or short distances. Here people need not to worry about renting a car immediately, they don’t have to go to shops to rent car according to their requirements. We provide services through our website so that customers can choose their vehicle according to their preferences and model. Customer can drive to their destination and have a peaceful ride without worrying about anything. Our aim is to provide best services to our customers so they can set back and relax.
+
+							</p>
+							<a class="text-uppercase primary-btn" href="contact.html">Contact Us</a>
+						</div>
+					</div>
+				</div>	
+			</section>
+			<!-- End home-about Area -->	
+
+			<!-- Start feature Area -->
+			<section class="feature-area section-gap" id="service">
+				<div class="container">
+					<div class="row d-flex justify-content-center">
+						<div class="col-md-8 pb-40 header-text">
+							<h1>What Services we offer to our clients</h1>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-lg-4 col-md-6">
+							<div class="single-feature">
+								<h4><span class="lnr lnr-user"></span>Expert Technicians</h4>
+								<p>
+									We have a team of expert technicians that who keep the car in top condition. They configure your car according to your requirements.
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="single-feature">
+								<h4><span class="lnr lnr-license"></span>Professional Service</h4>
+								<p>
+									We provide professional services to our esteemed customers. We always aim to satisfy the customer.
+								</p>								
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="single-feature">
+								<h4><span class="lnr lnr-phone"></span>Great Support</h4>
+								<p>
+									We have a expert support team who are ever ready to help you with anything. We provide 24/7 services to our esteemed customers.
+								</p>								
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="single-feature">
+								<h4><span class="lnr lnr-rocket"></span>Technical Skills</h4>
+								<p>
+									Highest level of technical engineers keep your car in top condition.
+								</p>				
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="single-feature">
+								<h4><span class="lnr lnr-diamond"></span>Highly Recomended</h4>
+								<p>
+									We have been highly recommended by many reputable magazines, online portals and newspapers. Along that we have a very good track record.
+								</p>								
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="single-feature">
+								<h4><span class="lnr lnr-bubble"></span>Positive Reviews</h4>
+								<p>
+									We always aim to give best services to our customers which is why we hold a very positive review from our clients.
+								</p>									
+							</div>
+						</div>						
+					</div>
+				</div>	
+			</section>
+			<!-- End feature Area -->		
+			
+			<!--Start Counter-->
+			<section class="facts-area section-gap" id="facts-area">
+				<div class="container">
+					<div class="row">
+						<div class="col single-fact">
+							<h1 class="counter">2536</h1>
+							<p>Projects Completed</p>
+						</div>
+						<div class="col single-fact">
+							<h1 class="counter">6784</h1>
+							<p>Really Happy Clients</p>
+						</div>
+						<div class="col single-fact">
+							<h1 class="counter">1059</h1>
+							<p>Total Tasks Completed</p>
+						</div>
+						<div class="col single-fact">
+							<h1 class="counter">2239</h1>
+							<p>Cups of Coffee Taken</p>
+						</div>
+						<div class="col single-fact">
+							<h1 class="counter">435</h1>
+							<p>In House Professionals</p>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!--ENd counter-->
+
+
+									
+
+			<!-- Start reviews Area -->
+			<section class="reviews-area section-gap " id="review ">
+				<div class="container ">
+					<div class="row d-flex justify-content-center ">
+						<div class="col-md-8 pb-40 header-text text-center ">
+							<h1>Customer Reviews</h1>
+							<p class="mb-10 text-center ">
+								Who are in extremely love with eco friendly system.
+							</p>
+						</div>
+					</div>					
+					<div class="row ">
+						<div class="col-lg-4 col-md-6 ">
+							<div class="single-review ">
+								<h4>Hari Shrestha</h4>
+								<p>
+									I have never in my life used a trust worthy and user friendly website like this before. They deliver in time and the cars are in the best conditions. Loved it.
+								</p>
+								<div class="star ">
+									<span class="fa fa-star checked "></span>
+									<span class="fa fa-star checked "></span>
+									<span class="fa fa-star checked "></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 ">
+							<div class="single-review ">
+								<h4>Chad Herrera</h4>
+								<p>
+										If you want a quick and hassel free car rental experience then you've come to the best place. I couldn't recommend any better service in Nepal at least.
+								</p>
+								<div class="star ">
+									<span class="fa fa-star checked "></span>
+									<span class="fa fa-star checked "></span>
+									<span class="fa fa-star checked "></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 ">
+							<div class="single-review ">
+								<h4>Pukar Chaudhary</h4>
+								<p>
+									A well balanced website. Great service.
+								</p>
+								<div class="star ">
+									<span class="fa fa-star checked "></span>
+									<span class="fa fa-star checked "></span>
+									<span class="fa fa-star checked "></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star "></span>
+								</div>
+							</div>
+						</div>							
+						<div class="col-lg-4 col-md-6 ">
+							<div class="single-review ">
+								<h4>Hardik Adhikari</h4>
+								<p>
+									Simply the best. No other words to describe this service.
+								</p>
+								<div class="star ">
+									<span class="fa fa-star checked "></span>
+									<span class="fa fa-star checked "></span>
+									<span class="fa fa-star checked "></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 ">
+							<div class="single-review ">
+								<h4>Libisa Dangol</h4>
+								<p>
+									A great customer care service with affordable price ranges. I would recommend this website to anyone looking to rent an affordable car.
+								</p>
+								<div class="star ">
+									<span class="fa fa-star checked "></span>
+									<span class="fa fa-star checked "></span>
+									<span class="fa fa-star checked "></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star "></span>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 ">
+							<div class="single-review ">
+								<h4>Sita Pradhan</h4>
+								<p>
+									A wide variety of cars and price ranges. One can find a car according to thier preference. I like to commend the customer serivce. A Class.
+								</p>
+								<div class="star ">
+									<span class="fa fa-star checked "></span>
+									<span class="fa fa-star checked "></span>
+									<span class="fa fa-star checked "></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+								</div>
+							</div>
+						</div>												
+					</div>
+				</div>	
+			</section>
+			<!-- End reviews Area -->
+			<!-- Start callaction Area -->
+			<section class="callaction-area relative section-gap ">
+				<div class="overlay overlay-bg "></div>
+				<div class="container ">
+					<div class="row justify-content-center ">
+						<div class="col-lg-10 ">
+							<h1 class="text-white ">Experience Great Support</h1>
+							<p>
+								You can experience great support form our support team who are always there to help you. We provide 24/7 service to our esteemed customers.
+							</p>
+							<a class="callaction-btn text-uppercase " href="contact.html ">Reach Our Support Team</a>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- End callaction Area -->
+
+			<!-- start footer Area -->
+			<footer class="footer-area section-gap ">
+				<div class="container ">
+					<div class="row ">
+			
+			
+						<div class="col-lg-2 col-md-6 col-sm-6 ">
+							<!--Add services-->
+						</div>
+						<div class="col-lg-2 col-md-6 col-sm-6 social-widget ">
+							<div class="single-footer-widget ">
+								<h6>Follow Us</h6>
+								<p>Let us be social</p>
+								<div class="footer-social d-flex align-items-center ">
+									<a href="# "><i class="fa fa-facebook "></i></a>
+									<a href="# "><i class="fa fa-twitter "></i></a>
+									<a href="# "><i class="fa fa-dribbble "></i></a>
+									<a href="# "><i class="fa fa-behance "></i></a>
+								</div>
+							</div>
+						</div>
+			
+						<p class="mt-50 mx-auto footer-text col-lg-12 ">
+							Copyright P4L Rent-A-Car Service 2019
+						</p>
+					</div>
+				</div>
+			</footer>
+			<!-- End footer Area -->
+
+			<script src="js/vendor/jquery-2.2.4.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+			<script src="js/vendor/bootstrap.min.js"></script>			
+			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+			<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>			
+  			<script src="js/easing.min.js"></script>			
+			<script src="js/hoverIntent.js"></script>
+			<script src="js/superfish.min.js"></script>	
+			<script src="js/jquery.ajaxchimp.min.js"></script>
+			<script src="js/jquery.magnific-popup.min.js"></script>	
+			<script src="js/owl.carousel.min.js"></script>			
+			<script src="js/jquery.sticky.js"></script>
+			<script src="js/jquery.nice-select.min.js"></script>	
+			<script src="js/waypoints.min.js"></script>
+			<script src="js/jquery.counterup.min.js"></script>					
+			<script src="js/parallax.min.js"></script>		
+			<script src="js/mail-script.js"></script>	
+			<script src="js/main.js"></script>	
+		</body>
+	</html>
+<?php /**PATH D:\Development\Rent-A-Car Project files\rent-a-car\resources\views/about.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/6e160ce8f44ebd12e369eafb23773c97012c0801.php
@@ -0,0 +1,1 @@
+<?php /**PATH D:\Development\Rent-A-Car Project files\rent-a-car\resources\views/login.blade.php ENDPATH**/ ?>
